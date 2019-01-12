573840902696e702b73f624a5df7673cc9b3ee9f
victorsilvasilva <victor.matheus@redes.unb.br>
2019-01-11 23:41:32 -0200

    Updates

---
+++ precache-manifest.d114bb196d11de79ab5655f613882530.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79a5f8e251dc3a98818e",
+    "url": "/feeltibet-site/static/js/runtime~main.79a5f8e2.js"
+  },
+  {
+    "revision": "f095e29b68338c7a43d4",
+    "url": "/feeltibet-site/static/js/main.f095e29b.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/feeltibet-site/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "f095e29b68338c7a43d4",
+    "url": "/feeltibet-site/static/css/main.11b8ab3f.chunk.css"
+  },
+  {
+    "revision": "490480b84079738092c7f4d09c1fb2ae",
+    "url": "/feeltibet-site/index.html"
+  }
+];
