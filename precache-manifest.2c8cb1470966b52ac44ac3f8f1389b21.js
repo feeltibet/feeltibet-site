1a26fd142d195e9123305f036afac241f911961d
Victor Silva <victorsilvafoto@gmail.com>
2019-01-26 00:49:17 -0200

    Updates

---
+++ precache-manifest.2c8cb1470966b52ac44ac3f8f1389b21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d1e691a45815218db45",
+    "url": "/static/js/main.0d1e691a.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "0d1e691a45815218db45",
+    "url": "/static/css/main.11b8ab3f.chunk.css"
+  },
+  {
+    "revision": "d4cd6f4ac77533d5d6ff81f79040de9a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/feeltibet-site/precache-manifest.5c26e638dbe3d03e58a72591eb45d053.js"
+  "/precache-manifest.2c8cb1470966b52ac44ac3f8f1389b21.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/feeltibet-site/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
