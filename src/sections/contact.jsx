af8e024326c03deb3830dd3770eca61efbd8e48a
Victor Silva <victorsilvafoto@gmail.com>
2019-02-09 13:40:45 -0200

    skype e widget do insta

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -7,8 +7,7 @@ export const Contact = (props) => {
                 Contact Us
         </h1>
             <div class="container">
-                <div class="row">{/* 
-                    <div class="map-wrap" style={{ width: '100%', height: 445 }} id="map"></div> */}
+                <div class="row" style={{ marginBottom: 50 }}>
                     <div class="col-lg-4 d-flex flex-column address-wrap">
                         <div class="single-contact-address d-flex flex-row">
                             <img style={{
@@ -16,7 +15,7 @@ export const Contact = (props) => {
                                 height: '25rem'
                             }} className="img-fluid" src="img/qr-code.jpeg" />
                         </div>
-                        <div className="row"><p style={{margin:'0 auto'}}>WeChat ID: feeltibet</p>
+                        <div className="row"><p style={{ margin: '0 auto' }}>WeChat ID: feeltibet</p>
                         </div>
                     </div>
                     <div class="col-lg-8">
@@ -38,6 +37,25 @@ export const Contact = (props) => {
                         </form>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-lg-4 d-flex flex-column address-wrap text-center">
+                        <h2>Address</h2>
+                        <p>Chimey Dolkar <br />
+                            Dosam sanglam chego 18<br />
+                            Beijing East road <br />
+                            850000 <br />
+                            Phone 13618913433, 15089020597</p>
+                    </div>
+                    <div class="col-lg-4 d-flex flex-column address-wrap text-center">
+
+                        <div style={{
+    margin: '0 auto'}} class="elfsight-app-064ea279-8c41-4c3f-9b36-d353218d4c5a"></div>
+                    </div>
+                    <div class="col-lg-4 d-flex flex-column address-wrap text-center">
+                        <h2>Skype</h2>
+                        <p>id: gonpoyonden</p>
+                    </div>
+                </div>
             </div>
-        </section>)
+        </section >)
 }
